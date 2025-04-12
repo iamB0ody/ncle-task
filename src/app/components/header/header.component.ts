1a81ff68bfdb37da76adce1dafe28710d5cbb027
Abdelrahman Ahmed <AbdElRahman.UI@gmail.com>
2025-04-12 18:10:27 +0200

    feat: add home route to header logo

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: 'header.component.html',
   styleUrls: ['header.component.scss'],
-  imports: []
+  imports: [RouterModule],
 })
 export class HeaderComoponent {
   constructor() {}
