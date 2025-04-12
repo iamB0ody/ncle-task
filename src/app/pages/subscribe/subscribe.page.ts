00927927672cd6d7a08718610ac38a391d22e0c3
Abdelrahman Ahmed <AbdElRahman.UI@gmail.com>
2025-04-12 16:11:57 +0200

    feat: subscribe page structure

--- src/app/pages/subscribe/subscribe.page.ts
+++ src/app/pages/subscribe/subscribe.page.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { IonContent } from '@ionic/angular/standalone';
-import { FooterComoponent } from 'src/app/components/footer/footer.component';
 import { HeaderComoponent } from 'src/app/components/header/header.component';
 
 @Component({
   selector: 'app-subscribe',
   templateUrl: 'subscribe.page.html',
   styleUrls: ['subscribe.page.scss'],
-  imports: [IonContent, FooterComoponent, HeaderComoponent],
+  imports: [IonContent, HeaderComoponent],
 })
 export class SubscribePage {
   constructor() {}
